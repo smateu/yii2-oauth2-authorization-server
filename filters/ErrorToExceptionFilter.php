24d4b58f2493dc2aca146029cbc6ec441dbe59bf
ldrc2895 <ldrc2895@gmail.com>
2018-01-18 12:46:57 -0600

    Error To Exception Filter fix

--- filters/ErrorToExceptionFilter.php
+++ filters/ErrorToExceptionFilter.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace filsh\yii2\oauth2server\filters;
-
 use Yii;
 use yii\base\Controller;
 use filsh\yii2\oauth2server\Module;
 use filsh\yii2\oauth2server\exceptions\HttpException;
-
 class ErrorToExceptionFilter extends \yii\base\Behavior
 {
     /**
@@ -16,7 +13,6 @@ class ErrorToExceptionFilter extends \yii\base\Behavior
     {
         return [Controller::EVENT_AFTER_ACTION => 'afterAction'];
     }
-
     /**
      * @param ActionEvent $event
      * @return boolean
@@ -25,7 +21,6 @@ class ErrorToExceptionFilter extends \yii\base\Behavior
     public function afterAction($event)
     {
         $response = Module::getInstance()->getServer()->getResponse();
-
         $isValid = true;
         if($response !== null) {
             $isValid = $response->isInformational() || $response->isSuccessful() || $response->isRedirection();
@@ -34,24 +29,12 @@ class ErrorToExceptionFilter extends \yii\base\Behavior
             throw new HttpException($response->getStatusCode(), $this->getErrorMessage($response), $response->getParameter('error_uri'));
         }
     }
-    
+
     protected function getErrorMessage(\OAuth2\Response $response)
     {
         $message = Module::t('common', $response->getParameter('error_description'));
         if($message === null) {
             $message = Module::t('common', 'An internal server error occurred.');
-        $response = Yii::$app->getModule('oauth2')->getServer()->getResponse();
-        $optional = $event->action->controller->getBehavior('authenticator')->optional;
-        $currentAction = $event->action->id;
-        $isValid = true;
-        if (!in_array($currentAction, $optional)) {
-            if ($response !== null) {
-                $isValid = $response->isInformational() || $response->isSuccessful() || $response->isRedirection();
-            }
-            if (!$isValid) {
-                throw new HttpException($response->getStatusCode(), $this->getErrorMessage($response),
-                    $response->getParameter('error_uri'));
-            }
         }
         return $message;
     }
